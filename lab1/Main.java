b1df649749b1aaccd730ac1ea4cbd3bf2477030e
M4SONY <42843149+M4SONY@users.noreply.github.com>
2019-02-22 10:45:32 +0200

    cleaned code
    cleaned code (Main)

--- lab1/Main.java
+++ lab1/Main.java
@@ -3,30 +3,30 @@ package ua.lvil.iot.lab1;
 public class Main {
 
 	public static void main(String[] args) {
-		Lamp Turbo = new Lamp();
+		Lamp turbo = new Lamp();
 
-		Lamp Enerlight = new Lamp("Enerlight", 60, "2 years", "Germany", 120, "LED", "black", 80);
+		Lamp enerlight = new Lamp("enerlight", 60, "2 years", "Germany", 120, "LED", "black", 80);
 
-		Lamp iLumia = new Lamp("iLumia", 60, "6 months", "Japan");
+		Lamp ilumia = new Lamp("ilumia", 60, "6 months", "Japan");
 
-		iLumia.setPrice(300);
-		iLumia.setType("LED");
-		iLumia.setColor("Grey");
-		iLumia.setSize(100);
+		ilumia.setPrice(300);
+		ilumia.setType("LED");
+		ilumia.setColor("Grey");
+		ilumia.setSize(100);
 
-		System.out.println(Turbo.toString());
-		System.out.println(Enerlight.toString());
-		System.out.println(iLumia.toString());
+		System.out.println(turbo.toString());
+		System.out.println(enerlight.toString());
+		System.out.println(ilumia.toString());
 
 		System.out.println("printStaticName: ");
-		Turbo.printStaticWight();
-		Enerlight.printStaticWight();
-		iLumia.printStaticWight();
+		turbo.printStaticWight();
+		enerlight.printStaticWight();
+		ilumia.printStaticWight();
 
 		System.out.println("printName");
-		Turbo.printWight();
-		Enerlight.printWight();
-		iLumia.printWight();
+		turbo.printWight();
+		enerlight.printWight();
+		ilumia.printWight();
 
 	}
 
